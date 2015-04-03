ed49ce80f12730e8fe624920ab843844e2e47f61
Nicolas Carlier <n.carlier@nunux.org>
2015-04-03 12:16:19 +0000

    fix: Update theme.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,12 +2,12 @@
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="clean"
+ZSH_THEME="gentoo"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git tmux zsh-syntax-highlighting)
+plugins=(git git-prompt tmux zsh-syntax-highlighting extract)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -21,4 +21,3 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-PROMPT="%{$fg[$NCOLOR]%}%B%n@%M%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(git_prompt_info)%(!.#.$) "
