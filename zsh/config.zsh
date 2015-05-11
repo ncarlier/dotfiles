772029d21fc96669e4a551c9ff93e4793d5b30be
Nicolas Carlier <nicolas.carlier@worldline.com>
2015-05-11 11:17:32 +0200

    chore(zsh): Add ~/bin to path.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,18 +1,3 @@
-# Set name of the theme to load.
-# Look in ~/.oh-my-zsh/themes/
-# Optionally, if you set this to "random", it'll load a random theme each
-# time that oh-my-zsh is loaded.
-ZSH_THEME="clean"
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git tmux zsh-syntax-highlighting)
-
-source $ZSH/oh-my-zsh.sh
-
-# Customize to your needs...
-
 fpath=($DOTFILES/functions $fpath)
 
 autoload -U $DOTFILES/functions/*(:t)
@@ -21,3 +6,6 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
+if [ -d ~/bin ]; then
+    export PATH=$PATH:~/bin
+fi
