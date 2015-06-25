0228d052201072d57bf2b8d26ee420c6ab4abc7e
Nicolas Carlier <n.carlier@nunux.org>
2015-06-25 17:24:40 +0000

    feat(ssh): Add config.

---
+++ ssh/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+fi
+
+if [! -f ~/.ssh/config ]; then
+    cp -r $(pwd)/ssh/files/config ~/.ssh/
+fi
+
+if [! -f ~/.ssh/environment ]; then
+    cp -r $(pwd)/ssh/files/environment ~/.ssh/
+fi
+
+
