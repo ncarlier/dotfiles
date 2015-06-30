621db77807f5ddbeebfa4c3cd7e4c41378ddfd04
Nicolas Carlier <n.carlier@nunux.org>
2015-06-30 14:06:00 +0200

    fix(ssh): Fix installation script.

--- ssh/install.sh
+++ ssh/install.sh
@@ -3,11 +3,11 @@ if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
 fi
 
-if [! -f ~/.ssh/config ]; then
+if [ ! -f ~/.ssh/config ]; then
     cp -r $(pwd)/ssh/files/config ~/.ssh/
 fi
 
-if [! -f ~/.ssh/environment ]; then
+if [ ! -f ~/.ssh/environment ]; then
     cp -r $(pwd)/ssh/files/environment ~/.ssh/
 fi
 
